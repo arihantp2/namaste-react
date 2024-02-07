90d5a119ed0f7061b3f82d8b671ac776f0307aea
arihantp2 <ap.machinelearning@gmail.com>
2024-02-07 21:58:52 +0530

    Lecture 2 added

---
+++ Lecture 2 - Igniting our App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I'm H1 Tag"),
+        React.createElement("h2", {}, "I'm H2 Tag")
+    ]
+    ), React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I'm H1 Tag"),
+        React.createElement("h2", {}, "I'm H2 Tag")
+    ]
+    )]
+)
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World From React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);
+root.render(heading);
+root.render(parent);
