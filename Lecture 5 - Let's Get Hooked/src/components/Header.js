a1e1e1dfa53767d4bc439d400c04671297c82e09
arihantp2 <ap.machinelearning@gmail.com>
2024-02-08 20:29:00 +0530

    Added Lecture 5 & Updated file structure

---
+++ Lecture 5 - Let's Get Hooked/src/components/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <img
+            className="logo"
+            src="https://play-lh.googleusercontent.com/QZFR_4xreirxaWya9C_SEzo0ZmIvh3so-PsM1gT3HthVWFGnYKodhn6DjtlvkBfYwQ"
+          />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contct Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  export default Header;
---
+++ Lecture 5 - Let's Get Hooked/src/components/RestaurantCard.js
@@ -0,0 +1,37 @@
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          cloudinaryImageId
+        }
+      />
+
+      <h3>{name}</h3>
+      <h4>{cuisines.join(" , ")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{costForTwo / 100} For Two</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
