524afa610774188ca317f2461145ed437be5ec19
arihantp2 <ap.machinelearning@gmail.com>
2024-02-08 22:28:40 +0530

    Filter logic on onclick

--- Lecture 5 - Let's Get Hooked/src/components/Body.js
+++ Lecture 5 - Let's Get Hooked/src/components/Body.js
@@ -2,11 +2,75 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "./utils/mockData";
 
 const Body = () => {
+  let ListOfRestaurants = [
+    {
+      data: {
+        id: "74453",
+        name: "Domino's Pizza",
+        uuid: "87727dbd-7f2b-4857-9763-359624165845",
+        city: "21",
+        area: "Athwa",
+        totalRatingsString: "1000+ ratings",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas", "Burgers", "Cold Drinks"],
+        costForTwo: 40000,
+        costForTwoString: "₹400 FOR TWO",
+        deliveryTime: 45,
+        avgRating: "3.5",
+      },
+    },
+    {
+      data: {
+        id: "74454",
+        name: "Domino's Pizza",
+        uuid: "87727dbd-7f2b-4857-9763-359624165845",
+        city: "21",
+        area: "Athwa",
+        totalRatingsString: "1000+ ratings",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas", "Burgers", "Cold Drinks"],
+        costForTwo: 40000,
+        costForTwoString: "₹400 FOR TWO",
+        deliveryTime: 45,
+        avgRating: "3.0",
+      },
+    },
+    {
+      data: {
+        id: "74455",
+        name: "Domino's Pizza",
+        uuid: "87727dbd-7f2b-4857-9763-359624165845",
+        city: "21",
+        area: "Athwa",
+        totalRatingsString: "1000+ ratings",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas", "Burgers", "Cold Drinks"],
+        costForTwo: 40000,
+        costForTwoString: "₹400 FOR TWO",
+        deliveryTime: 45,
+        avgRating: "4.5",
+      },
+    },
+  ];
+  
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            ListOfRestaurants = ListOfRestaurants.filter(
+              (res) => res.data.avgRating > 3
+            );
+
+            console.log(ListOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {ListOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
