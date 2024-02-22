a2dd01717ad86d241db74bd7d749bf00e0284aff
arihantp2 <ap.machinelearning@gmail.com>
2024-02-22 23:45:27 +0530

    Added Lecture 7 Initial code

---
+++ Lecture 7 - Finding the path/src/components/Body.js
@@ -0,0 +1,86 @@
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
+
+const Body = () => {
+  const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredtRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    // optional chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return ListOfRestaurants && ListOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+
+          <button
+            onClick={() => {
+              const filteredtRestaurants = ListOfRestaurants.filter((res) => {
+                return res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              setFilteredRestaurants(filteredtRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ListOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+
+            setFilteredRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
+      <div className="res-container">
+        {filteredtRestaurants.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant?.info?.id}
+            resData={restaurant?.info}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ Lecture 7 - Finding the path/src/components/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://play-lh.googleusercontent.com/QZFR_4xreirxaWya9C_SEzo0ZmIvh3so-PsM1gT3HthVWFGnYKodhn6DjtlvkBfYwQ";
