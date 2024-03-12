2fe040d0f030af1fc7fc3f56a9246bbe97dc85e4
arihantp2 <ap.machinelearning@gmail.com>
2024-03-12 20:47:18 +0530

    Lecture 12 initial setup

---
+++ Lecture 12 - Let's build our store/src/components/Error.js
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError(); // To get information about errors in routing1
+  // console.log(err);
+
+  return (
+    <div>
+      <h1>Oops!!!</h1>
+      <h2>Something went wrong!</h2>
+      <h3>
+        {err.status} : {err.statusText}
+      </h3>
+    </div>
+  );
+};
+
+export default Error;
